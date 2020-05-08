23f16c55229f226ffc77bfbb747a976cb8a9abef
Robert Candler <robert.candler.32@gmail.com>
2020-05-07 22:09:47 -0600

    routes completed

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,40 +1,52 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts");
 
+//get, post, post from app.js
 
-
-
+//get last workout
 app.get("/api/workouts", (req, res) => {
-    Workout.find()
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  Workout.find()
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
   });
+});
+//put add exercise (unfinished)
+app.put("/api/workouts/", (req, res) => {
+  Workout.findOneAndUpdate(req.params, {
+      new: true,
+      upsert: true
+  })
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
 
-  app.put("/api/workouts/", (req, res) => {
-    Workout.findOneAndUpdate()
-    .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
-        res.json(err);
-    });
+//post create workout
+app.post("/submit", ({body}, res) => {
+  Workout.create(body)
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
   });
-  
-  app.get("/api/workouts/range", (req, res) => {
-    Workout.find({}).limit(6)
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+});
+
+//get workouts in range
+app.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(5) //out of 10
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
   });
-  
-  app.listen(3000, () => {
-    console.log("App running on port 3000!");
-  });
+});
+
   
